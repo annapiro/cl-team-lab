e0a16e277167f2b093aaf89b0ccfe59e3eb6841d
silverymist <26202085+silverymist@users.noreply.github.com>
2023-04-24 10:42:12 +0200

    Restaurant stub

---
+++ Restaurant.py
@@ -0,0 +1,10 @@
+class Restaurant:
+    def __init__(self):
+        self.name = ''
+        self.price = ''
+        self.category = ''
+        self.location = ''
+        self.menu = []
+
+    def read_file(self, filename):
+        pass
